2b20e197d7feec8206a8cca89218c07952ccdb54
rbezerrajc <rbezerra@sjcc.com.br>
2024-05-13 21:31:16 -0300

    finalizando

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+const buttonPlay = document.querySelector('#play');
+const buttonPause = document.querySelector('#pause');
+const tempoAtual = document.getElementById("tempoAtual");
+const tempoTotal = document.getElementById("tempoTotal");
+
+const music = new Audio('./assets/Pra-onde-eu-irei.mp3');
+let interval;
+
+//funçoes
+
+function formatarTempo(segundos) {
+  const min = Math.floor(segundos / 60);
+  const seg = Math.floor(segundos % 60);
+  return `${min.toString().padStart(2, '0')}:${seg.toString().padStart(2, '0')}`;
+}
+
+function updateMusicTime() {
+  const tempoAtual = document.getElementById('tempoAtual');
+  const progressBar = document.getElementById("progressBar");
+  const progresso = (music.currentTime / music.duration) * 100;
+  progressBar.value = progresso;
+  tempoAtual.textContent = formatarTempo(music.currentTime);
+}
+
+
+music.addEventListener('loadedmetadata', function () {
+  const tempoTotal = document.getElementById('tempoTotal');
+  tempoTotal.textContent = formatarTempo(music.duration);
+});
+
+function play() {
+  buttonPlay.classList.add('hide');
+  buttonPause.classList.remove('hide');
+  music.play();
+
+  interval = setInterval(updateMusicTime, 1000);
+}
+function pause() {
+  buttonPlay.classList.remove('hide');
+  buttonPause.classList.add('hide');
+  music.pause();
+}
+
+buttonPlay.addEventListener('click', play);
+buttonPause.addEventListener('click', pause);
